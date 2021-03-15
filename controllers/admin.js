fd05334adc7621ceb6f954271148c43d7cd9a5c5
Rhushabh1 <rhushabhgedam3@gmail.com>
2021-03-16 04:15:28 +0530

    temp

--- controllers/admin.js
+++ controllers/admin.js
@@ -10,7 +10,6 @@ exports.get_test = (req,res,next) => {
         editing: false
     });
 
-
 };
 
 exports.post_test = (req,res,next) => {
@@ -47,17 +46,18 @@ exports.get_prods = (req,res2,next) => {
 };
 
 exports.post_prods = (req,res,next) => {
-    const title = req.body.title;
-    const image = req.body.image
-    const price = req.body.price;
-    const quantity = req.body.quantity;
-    const product = new Prod( title, image, price,quantity);
-    product
-        .add_prod()
-        .then(() => {
-            res.redirect('/prods');
-        })
-        .catch(err => console.log(err));
+    const user_id = 1;
+    const item_id = req.body.id;
+    const quantity = 1;
+
+    const cart = new Cart( user_id, item_id, quantity );
+    const ret = cart.add_to_cart();
+    if (ret == 0) {
+        res.redirect('/cart');
+    } else {
+        res.redirect('/prods');
+    }
+        
 };
 
 exports.get_cart = (req,res,next) => {
--- models/prod.js
+++ models/prod.js
@@ -1,6 +1,7 @@
 
 const pool= require('../utils/database');
-module.exports = class Prod{
+
+class Prod{
 
     constructor( title, image, price, quantity){
         this.title = title;
@@ -10,16 +11,48 @@ module.exports = class Prod{
     }
 
     add_prod(){
-        return pool.query('INSERT INTO products(title, price, image, quantity, id) VALUES ($1, $2, $3, $4, (SELECT max(id)+1 from products));', [this.title, this.price, this.image, this.quantity]);
+        return pool.query('INSERT INTO products(title, price, image, quantity) VALUES ($1, $2, $3, $4);', [this.title, this.price, this.image, this.quantity]);
     }
     static get_all(){
         return pool.query('SELECT * FROM products');
-        // pool.query("SELECT * from some_table", (err, res) => {
-        //     console.log(res);
-        //   });
-        // return res;
     }
 
-    
-
 };
+
+class Cart{
+
+    constructor( user_id, item_id, quantity ){
+        this.user_id = user_id;
+        this.item_id = item_id;
+        this.quantity = quantity;
+    }
+
+    add_to_cart(){
+        const q1 = pool.query('SELECT * FROM products WHERE id = $1', [this.item_id]);
+        
+        q1.then(res => {
+            const tuple = res.rows;
+            console.log(tuple);
+
+            if (tuple[0]['quantity'] - this.quantity >= 0) {
+                const q2 = pool.query('UPDATE products SET quantity = quantity - $1 where id = $2; INSERT INTO cart (user_id, item_id, quantity) VALUES ($3, $2, $1) ON DUPLICATE KEY UPDATE quantity = quantity + $1;', [this.quantity, this.item_id, this.user_id]);
+                
+                q2.then(() => {
+                    return 0;
+                })
+                .catch(err => console.log(err));
+
+            } else {
+                console.log("add to cart failed");
+                return -1;
+            }
+        })
+        .catch(err => console.log(err));
+    }
+
+}
+
+module.exports = {
+    Prod: Prod,
+    Cart: Cart
+}
